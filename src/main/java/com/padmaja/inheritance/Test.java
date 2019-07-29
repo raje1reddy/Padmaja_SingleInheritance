391cf1dfd85c445e7295bb819a20d4099ec9bc06
unknown <raje1reddy@gmail.com>
2019-07-29 11:54:42 +0100

    my initial code for SingleInheritance

---
+++ src/main/java/com/padmaja/inheritance/Test.java
@@ -0,0 +1,21 @@
+package com.padmaja.inheritance;//Java program to illustrate the
+// concept of inheritance 
+
+
+
+
+
+// driver class 
+public class Test 
+{ 
+	public static void main(String args[]) 
+	{ 
+		
+		//MountainBike mb = new MountainBike(3, 100, 25);
+		//System.out.println(mb.toString());
+		Bicycle b = new MountainBike(3, 100, 25);
+		b.toString();
+		System.out.println(b.toString());
+			
+	} 
+} 
